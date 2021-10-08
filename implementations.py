11d7fde89ae2985a5cdc5a1a1e69d35ed64bd157
Matteo Calafa <matteo.calafa@mail.polimi.it>
2021-10-08 09:20:35 +0000

    Added methods from LAB 3, corrected methods from LAB 2

--- implementations.py
+++ implementations.py
@@ -4,25 +4,31 @@ import matplotlib.pyplot as plt
 # PLEASE, CHECK ALL THE IMPLEMENTATIONS
 # Functions needed for the project (see "Step 2" in "project1_description.pdf")
 
-# Methods taken from lab 2, Matteo
+
+# Methods from LAB 2
+
+def compute_loss_MSE(y, tx, w):
+    e = y - tx @ w
+    N = len(y)
+    return (e**2).sum()/(2*N)
+
 
 def compute_gradient_MSE(y, tx, w):
     e = y - tx @ w
-    N = y.shape[0]
-    g0 = -(1/N)*(e.T @ tx[:,0])
-    g1 = -(1/N)*(e.T @ tx[:,1])
-    return np.array([g0, g1])
+    N = len(y)
+    g = -(1/N)*(tx.T @ e)
+    return g
 
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     w = initial_w
     for n_iter in range(max_iters):
         w = w - gamma*compute_gradient_MSE(y,tx,w)
-    loss = compute_loss(y, tx, w)
+    loss = compute_loss_MSE(y, tx, w)
     return w,loss
 
 
-def stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma):
+def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
     w = initial_w
     g = 0
     for n_iter in range(max_iters):
@@ -30,5 +36,23 @@ def stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma):
         for b_y, b_x in batch_iter(y, tx, 1):  # since n_batches is by default = 1, this loop is done only once
             g = gamma * compute_gradient_MSE(b_y, b_x, w)
         w = w - g
-    loss = compute_loss(y, tx, w)
+    loss = compute_loss_MSE(y, tx, w)
     return w,loss
+
+
+# Methods from LAB 3
+
+def least_squares(y, tx):
+    w = np.linalg.solve(tx.T @ tx, tx.T @ y)
+    loss = compute_loss_MSE(y,tx,w)
+    return w,loss
+
+
+def ridge_regression(y, tx, lambda_):
+    N = len(y)
+    D = tx.shape[1]
+    I = np.eye(D)
+    w = np.linalg.solve(tx.T @ tx + 2*N*lambda_*I, tx.T @ y)
+    loss = compute_loss_MSE(y, tx, w)
+    return w,loss
+
