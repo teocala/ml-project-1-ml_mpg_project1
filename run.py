23270aad5f927d43cdcc249900f11357df33a849
Giulia Mescolini <65012808+giuliamesc@users.noreply.github.com>
2021-10-15 11:37:57 +0200

    added test loading, output generation

--- run.py
+++ run.py
@@ -2,8 +2,7 @@
 Main script to work and get results from the dataset
 """
 
-
-"""Libraries importation"""
+# Libraries importation
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,12 +10,11 @@ from proj1_helpers import *
 from implementations import *
 
 
-
 """ MAIN """
 
 if __name__ == '__main__':
 
-    """ Data loading (lines from ML_course/projects/project1/scripts/project1.ipynb"""
+    # Data loading
 
     # TODO: In the same directory of your repository folder, create a folder "data" where you place the data downloaded from
     #  1) https://github.com/epfml/ML_course/tree/master/projects/project1/data
@@ -27,3 +25,12 @@ if __name__ == '__main__':
     y, tX, ids = load_csv_data(DATA_TRAIN_PATH)
     N = len(y) # training set cardinality
     D = tX.shape[1] # number of parameters ("dimensionality")
+
+    
+    DATA_TEST_PATH = '../data/test.csv' 
+    _, tX_test, ids_test = load_csv_data(DATA_TEST_PATH)
+    
+    # Creation of the submission file
+    OUTPUT_PATH = '../data/submission.csv' 
+    y_pred = predict_labels(weights, tX_test)
+    create_csv_submission(ids_test, y_pred, OUTPUT_PATH)
